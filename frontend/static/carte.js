bed9a86ca8676c5c6c2d3daa54f19a73a47d552a
Anis BOUAZIZ <anis.bouaziz@u-psud.fr>
2020-05-27 00:13:56 +0200

    map

--- frontend/static/carte.js
+++ frontend/static/carte.js
@@ -24,10 +24,13 @@ function dess(x,y,ln,lg) {
  let cal_rad=function(degres){
      return degres / (180/Math.PI)
  }
+ let ln=function(x) {
+  return Math.log(x) / Math.LN2;
+}
  
  let cal_position = function(lat,lon,canvas){
-     let x =canvas.width * (lon + 180 ) / (360 - 30);
-     let y =Math.log(Math.tan(Math.PI/4 + cal_rad(lat)/2));
+     let x =canvas.width * (lon + 180 ) / (360);
+     let y =ln(Math.tan(Math.PI/4 + cal_rad(lat)/2));
      y=canvas.height /2 - canvas.width* y /(2 * Math.PI)
      let dot ={"x":x,"y":y};
      return dot;
@@ -44,9 +47,8 @@ function dess(x,y,ln,lg) {
  let create_point=function(lat,lon,size,canvas){
      let pos=cal_position(lat,lon,canvas);
      var ctx = canvas.getContext('2d');
-     var ctx = canvas.getContext('2d');
 
-    dess(pos.x,pos.y, 20,20);
+    dess(pos.x,pos.y, 10,10);
 
     /*
      let pt=document.createElement("div");
@@ -63,9 +65,14 @@ function dess(x,y,ln,lg) {
      
      var canvas=document.getElementById("carte");
      var context=canvas.getContext('2d');
+     
+     context.translate(-45,55);
+     dess(0,0, 10,10);
         
-     for (let i=0;i<tab.length;i++){
+     for (let i=0;i<tab.length;i++){    
+         console.log(tab[i][0],tab[i][1]);
          create_point(tab[i][0],tab[i][1],3,canvas)
+         console.log(cal_position(tab[i][0],tab[i][1],canvas));     
      }
      
      
